4d11199b0c3c7b9bf7ead568880bf78389a520da
k-yakuniku <game0game7@gmail.com>
2024-04-16 00:16:30 +0900

    se10 commit

--- dist/lib/Passport/local.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { Strategy } from 'passport-local';
-declare const LocalStrategy_base: new (...args: any[]) => Strategy;
-export declare class LocalStrategy extends LocalStrategy_base {
-    constructor();
-    validate(email: string, password: string): Promise<import("../Mongodb/users").IUsersType & Required<{
-        _id: string;
-    }>>;
-}
-export {};
