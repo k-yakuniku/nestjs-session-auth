25b98ffe0e0b1a196365cdaae8126709a2c15b1b
k-yakuniku <game0game7@gmail.com>
2024-04-15 21:13:34 +0900

    se6 commit

--- src/lib/Mongodb/users.js
+++ src/lib/Mongodb/users.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userModel = void 0;
-var mongoose_1 = require("mongoose");
-var uuid_1 = require("uuid");
-var userSchema = new mongoose_1.Schema({
+const mongoose_1 = require("mongoose");
+const uuid_1 = require("uuid");
+const userSchema = new mongoose_1.Schema({
     _id: { type: String, default: (0, uuid_1.v4)() },
     name: { type: String, required: false },
     introduction: { type: String, required: false },
@@ -13,3 +13,4 @@ var userSchema = new mongoose_1.Schema({
     updatedAt: { type: Date, default: Date.now() },
 });
 exports.userModel = (0, mongoose_1.model)('user', userSchema);
+//# sourceMappingURL=users.js.map
